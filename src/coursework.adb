94ac2157d8a1af02dab4dd7a8bd2e032d97ed302
Monica Richardson <MonicaR@forrit.com>
2021-05-18 22:51:52 +0100

    Added Invariant function which ensures cockpit and external doors are shut and locked and that there is a minimum amount of fuel onboard before takeoff. Added TakeOff procedure with pre and post conditions.

--- src/coursework.adb
+++ src/coursework.adb
@@ -2,6 +2,10 @@ package body coursework
 with SPARK_Mode
 is
 
+   procedure TakeOff is
+   begin
+      Plane := TakingOff;
+   end TakeOff;
    
 
 end coursework;
--- src/coursework.ads
+++ src/coursework.ads
@@ -1,7 +1,31 @@
 package coursework 
 with SPARK_Mode
 is
-
+   type DoorPosition is (Open, Shut);
+   type LockState is (Locked, Unlocked);
+   type FlightMode is (Stationary, UnderTow, TakingOff, NormalFlight, Landing);
+   type FuelLevel is range 1..26020;
    
+   Cockpit : DoorPosition;
+   ExternalDoors : DoorPosition;
+   CockpitLock : LockState;
+   ExternalDoorLocks : LockState;
+   Plane : FlightMode;
+   FuelOnboard : FuelLevel;
+   
+   MINFUEL : constant := 13986;
+   
+   function Invariant return Boolean is
+     (((Cockpit = Shut and CockpitLock = Locked)
+      and (ExternalDoors = Shut and ExternalDoorLocks = Locked)
+      and (FuelOnboard >= MINFUEL)) or (Plane = Stationary));
+   
+   procedure TakeOff with
+     Global => (Proof_In => (Cockpit,CockpitLock,ExternalDoors,ExternalDoorLocks, FuelOnboard),
+                In_Out => Plane),
+     Pre => Invariant and ((Cockpit = Shut and CockpitLock = Locked)
+                      and (ExternalDoors = Shut and ExternalDoorLocks = Locked)
+                      and (FuelOnboard >= MINFUEL)),
+     Post => Invariant and (Plane = TakingOff);
 
 end coursework;
