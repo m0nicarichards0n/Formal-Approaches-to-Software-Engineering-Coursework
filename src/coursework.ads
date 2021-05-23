f3718f6ddb5f7cd49eeac876bd8ddb989ac196be
Monica Richardson <MonicaR@forrit.com>
2021-05-23 17:49:19 +0100

    Created MasterInvariant and TakeOffInvariant, and TakeOff procedure which uses both of these.

--- src/coursework.ads
+++ src/coursework.ads
@@ -3,8 +3,11 @@ with SPARK_Mode
 is
    type DoorPosition is (Open, Shut);
    type LockState is (Locked, Unlocked);
+   type EngineState is (On, Off);
+   type LandingGearState is (Up, Down);
    type FlightMode is (Stationary, UnderTow, TakingOff, NormalFlight, Landing);
    type FuelLevel is range 1..26020;
+   type Altitude is range 1..60000;
    
    Cockpit : DoorPosition;
    ExternalDoors : DoorPosition;
@@ -12,20 +15,34 @@ is
    ExternalDoorLocks : LockState;
    Plane : FlightMode;
    FuelOnboard : FuelLevel;
+   Engine: EngineState;
+   CurrentAltitude : Altitude;
+   LandingGear : LandingGearState;
    
    MINFUEL : constant := 13986;
+   LOWALTITUDE : constant := 2000;
    
-   function Invariant return Boolean is
+   function MasterInvariant return Boolean is
      (((Cockpit = Shut and CockpitLock = Locked)
-      and (ExternalDoors = Shut and ExternalDoorLocks = Locked)
-      and (FuelOnboard >= MINFUEL)) or (Plane = Stationary));
+       and (ExternalDoors = Shut and ExternalDoorLocks = Locked))
+       or (Plane = Stationary));
+      
+   function TakeOffInvariant return Boolean is
+     (((FuelOnboard >= MINFUEL and Engine = On)
+       and (CurrentAltitude < LOWALTITUDE and LandingGear = Down))
+       or (Plane /= TakingOff));
    
    procedure TakeOff with
-     Global => (Proof_In => (Cockpit,CockpitLock,ExternalDoors,ExternalDoorLocks, FuelOnboard),
+     Global => (Proof_In => (Cockpit,CockpitLock,ExternalDoors,ExternalDoorLocks, 
+                             FuelOnboard, Engine, CurrentAltitude, LandingGear),
                 In_Out => Plane),
-     Pre => Invariant and ((Cockpit = Shut and CockpitLock = Locked)
-                      and (ExternalDoors = Shut and ExternalDoorLocks = Locked)
-                      and (FuelOnboard >= MINFUEL)),
-     Post => Invariant and (Plane = TakingOff);
+     Pre => MasterInvariant and TakeOffInvariant 
+     and ((Cockpit = Shut and CockpitLock = Locked)
+     and (ExternalDoors = Shut and ExternalDoorLocks = Locked))
+     and ((FuelOnboard >= MINFUEL and Engine = On)
+     and (CurrentAltitude < LOWALTITUDE and LandingGear = Down)),
+     Post => MasterInvariant and TakeOffInvariant and (Plane = TakingOff);
+ 
+   
 
 end coursework;
